783b69952882727127175a64fcda59fb0c668ac6
suryam0801 <58582997+suryam0801@users.noreply.github.com>
2021-12-22 22:56:32 -0800

    minor changes

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { GetUserType } from "../../Actions/Common/GetUserTypeActions";
 import AppointmentsTable from "../Appointments/AppointmentsTable"
 import DatePickerComponent from "../../Components/CalendarPicker/DatePickerComponent"
+import OrganisationPicker from "../OrganisationPicker/OrganisationPicker";
 import "../../Styles/main_page.css"
 
 function Dashboard() {
@@ -19,6 +20,7 @@ function Dashboard() {
   return (
     <>
       <div className="rowWith20PXGap">
+        <OrganisationPicker></OrganisationPicker>
         <AppointmentsTable></AppointmentsTable>
         <DatePickerComponent></DatePickerComponent>
       </div>
--- src/Reducers/OrganisationsReducer.ts
+++ src/Reducers/OrganisationsReducer.ts
@@ -2,25 +2,32 @@ import { Action } from "../Types/ActionType"
 import IOrganisationData from "../Types/Organisation";
 
 export enum Org_Types {
-    SET_LOCAL_ORGS = "SET_LOCAL_ORGS"
+  SET_LOCAL_ORGS = "SET_LOCAL_ORGS",
+  SET_LOCAL_SELECTED_ORG = "SET_LOCAL_SELECTED_ORG"
 }
 
 export interface OrganisationState {
-    organisations: IOrganisationData[]
+  organisations: IOrganisationData[];
+  selectedOrganisation?: IOrganisationData
 }
 
-const initialState:OrganisationState = {
-    organisations: []
+const initialState: OrganisationState = {
+  organisations: []
 }
 
-export const orgReducer = (state: OrganisationState = initialState, action: Action) : OrganisationState => {
-    switch (action.type) {
-      case Org_Types.SET_LOCAL_ORGS:
-        return {
-          organisations: action.payload,
-        }
-      default:
-        return state
-    }
-  };
-  
+export const orgReducer = (state: OrganisationState = initialState, action: Action): OrganisationState => {
+  switch (action.type) {
+    case Org_Types.SET_LOCAL_ORGS:
+      return {
+        ...state,
+        organisations: action.payload,
+      }
+    case Org_Types.SET_LOCAL_SELECTED_ORG:
+      return {
+        ...state,
+        selectedOrganisation: action.payload,
+      }
+    default:
+      return state
+  }
+};
