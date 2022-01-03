1e974c72c3ae2967f7bae591e88cb25a50974f17
suryam0801 <58582997+suryam0801@users.noreply.github.com>
2022-01-03 09:25:16 -0800

    minor fix

--- src/Helpers/OrganisationHelpers.ts
+++ src/Helpers/OrganisationHelpers.ts
@@ -11,6 +11,7 @@ export function checkForDefaultOrgHelpers(organisations:IOrganisationBasic[]) : 
     });
 
     if (!hasDefaultOrg) {
+        //LOG: Does not have default org. Incoming org list: {null empty or length}
         return null;
     } else {
         return defaultOrg;
