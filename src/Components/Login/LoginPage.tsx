9119e6aadf6c935bf4e4aba0bdb1df53f19201bb
suryam0801 <58582997+suryam0801@users.noreply.github.com>
2022-01-15 20:39:38 -0800

    minor fix

--- src/Components/Login/LoginPage.tsx
+++ src/Components/Login/LoginPage.tsx
@@ -1,14 +1,10 @@
-import { TextField } from "@mui/material";
 import { ChangeEvent, useState } from "react";
-import { Button, Col, Row } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { SignInWithPhoneNumberHelper } from "../../Actions/Common/LoginActions";
 import "../../Styles/login.css";
-import InputAdornment from '@mui/material/InputAdornment';
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 
 export default function LoginPage() {
-
+  
   const dispatch = useDispatch();
   const [phoneNumber, setPhoneNumber] = useState("");
 
@@ -21,38 +17,18 @@ export default function LoginPage() {
   };
 
   return (
-    <Row style={{ marginRight: 5 }}>
-      <Col md="4"></Col>
-      <Col>
-        <TextField
-          fullWidth
-          label="Phone Number"
-          placeholder="Phone Number"
-          margin="normal"
-          size="small"
-          required
-          value={phoneNumber}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <LocalPhoneIcon style={{ width: 22, height: 22 }}></LocalPhoneIcon>
-              </InputAdornment>
-            ),
-          }}
-          inputProps={{ maxLength: 10 }}
-          variant="outlined"
+    <div className="container">
+      <div>
+        <label>PhoneNumber</label>
+        <input
+          type="tel"
           onChange={onPhoneNumberChange}
+          placeholder="phonenumber"
         />
-
-        <Button
-          style={{ padding: 10, width: '100%' }}
-          type="submit"
-          color="primary"
-          onClick={() => verifyPhoneNumber()}>
-          Verify Number
-        </Button>
-      </Col>
-      <Col md="4"></Col>
-    </Row>
+      </div>
+      <button id="sign-in-button" onClick={verifyPhoneNumber}>
+        Verify Number
+      </button>
+    </div>
   );
 }
