48a9df839ebe4f2007fc02b0a16d5b80ee5f3358
Pravallika <05b.pravallika@gmail.com>
2022-01-24 14:15:46 +0530

    image display in horizontal

--- src/Components/PrescriptionUpload/PrescriptionImageView.tsx
+++ src/Components/PrescriptionUpload/PrescriptionImageView.tsx
@@ -1,95 +1,117 @@
-import { useState, useCallback, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import ImageViewer from 'react-simple-image-viewer';
-import { RootState } from '../../store';
-import CancelIcon from '@mui/icons-material/Cancel';
-import { DeletePrescription } from '../../ServiceActions/PrescriptionActions';
-import IPrescriptionIncomingData from '../../Types/IncomingDataModels/PrescriptionIncoming';
+import { useState, useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ImageViewer from "react-simple-image-viewer";
+import { RootState } from "../../store";
+import CancelIcon from "@mui/icons-material/Cancel";
+import { DeletePrescription } from "../../ServiceActions/PrescriptionActions";
+import IPrescriptionIncomingData from "../../Types/IncomingDataModels/PrescriptionIncoming";
 
 export default function PrescriptionImageView() {
-    const dispatch = useDispatch();
-    const [currentImage, setCurrentImage] = useState(0);
-    const [isViewerOpen, setIsViewerOpen] = useState(false);
-    const [images, setImages] = useState([""]);
-    let displayImageObjs = useSelector((state: RootState) => state.ConsultationState.currentCustomerPrescriptions);
+  const dispatch = useDispatch();
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isViewerOpen, setIsViewerOpen] = useState(false);
+  const [images, setImages] = useState([""]);
+  let displayImageObjs = useSelector(
+    (state: RootState) => state.ConsultationState.currentCustomerPrescriptions
+  );
 
-    const openImageViewer = useCallback((index) => {
-        setCurrentImage(index);
-        setIsViewerOpen(true);
-    }, []);
+  const openImageViewer = useCallback((index) => {
+    setCurrentImage(index);
+    setIsViewerOpen(true);
+  }, []);
 
-    const closeImageViewer = () => {
-        setCurrentImage(0);
-        setIsViewerOpen(false);
-    };
+  const closeImageViewer = () => {
+    setCurrentImage(0);
+    setIsViewerOpen(false);
+  };
 
-    useEffect(() => {
-        getImageURLsFromPrescriptions()
-    }, [displayImageObjs])
+  useEffect(() => {
+    getImageURLsFromPrescriptions();
+  }, [displayImageObjs]);
 
-    function getImageURLsFromPrescriptions() {
-        var stringList: string[] = [];
+  function getImageURLsFromPrescriptions() {
+    var stringList: string[] = [];
 
-        if (displayImageObjs) {
-            displayImageObjs.forEach(element => {
-                stringList.push(element.sasUrl);
-            });
-        }
-
-        setImages(stringList);
+    if (displayImageObjs) {
+      displayImageObjs.forEach((element) => {
+        stringList.push(element.sasUrl);
+      });
     }
 
-    function deletePrescription (prescription: IPrescriptionIncomingData) {
-        if (window.confirm("Are you sure you want to delete this prescription?")) {
-            dispatch(DeletePrescription(prescription))
-        }
+    setImages(stringList);
+  }
+
+  function deletePrescription(prescription: IPrescriptionIncomingData) {
+    if (window.confirm("Are you sure you want to delete this prescription?")) {
+      dispatch(DeletePrescription(prescription));
     }
+  }
 
-    function imageViewDisplay() {
-        return (
-            <div style={{ overflow: 'auto' }}>
-                {displayImageObjs && displayImageObjs.map((src, index) => (
-
-                    <div style={{ position: 'relative', width: 200, marginTop: 10 }}>
-                        <img
-                            src={src.sasUrl}
-                            onClick={() => openImageViewer(index)}
-                            width="200"
-                            key={index}
-                            style={{ margin: '5px' }}
-                            alt=""
-                        />
-
-                        <div onClick={() => dispatch(deletePrescription(src))} style={{ position: 'absolute', top: -10, right: -15 }}>
-                            {<CancelIcon></CancelIcon>}
-                        </div>
-                    </div>
-                ))}
-
-                {isViewerOpen && (
-                    <ImageViewer
-                        src={images}
-                        currentIndex={currentImage}
-                        disableScroll={false}
-                        closeOnClickOutside={true}
-                        onClose={closeImageViewer}
-                    />
-                )}
-            </div>
-        );
-    }
-
-    function noPrescriptionsDisplay() {
-        return (
-            <div style={{ margin: 20, marginTop: 50, marginBottom: 50, textAlign: 'center' }}>
-                No prescriptions yet. Upload image or Take Photo.
-            </div>
-        );
-    }
-
+  function imageViewDisplay() {
     return (
-        <div>
-            {(displayImageObjs && displayImageObjs.length > 0) ? imageViewDisplay() : noPrescriptionsDisplay()}
-        </div>
+      <div style={{ overflow: "auto" }}>
+        {displayImageObjs &&
+          displayImageObjs.map((src, index) => (
+            <div
+              style={{
+                display: "inline-block",
+                position: "relative",
+                width: 200,
+                margin: "10px 10px 0 0",
+              }}
+            >
+              <img
+                src={src.sasUrl}
+                onClick={() => openImageViewer(index)}
+                width="200"
+                height="120"
+                key={index}
+                style={{ margin: "5px" }}
+                alt=""
+              />
+
+              <div
+                onClick={() => dispatch(deletePrescription(src))}
+                style={{ position: "absolute", top: -10, right: -15 }}
+              >
+                <CancelIcon />
+              </div>
+            </div>
+          ))}
+
+        {isViewerOpen && (
+          <ImageViewer
+            src={images}
+            currentIndex={currentImage}
+            disableScroll={false}
+            closeOnClickOutside={true}
+            onClose={closeImageViewer}
+          />
+        )}
+      </div>
     );
+  }
+
+  function noPrescriptionsDisplay() {
+    return (
+      <div
+        style={{
+          margin: 20,
+          marginTop: 50,
+          marginBottom: 50,
+          textAlign: "center",
+        }}
+      >
+        No prescriptions yet. Upload image or Take Photo.
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {displayImageObjs && displayImageObjs.length > 0
+        ? imageViewDisplay()
+        : noPrescriptionsDisplay()}
+    </div>
+  );
 }
--- src/Components/ReportUpload/ReportImageView.tsx
+++ src/Components/ReportUpload/ReportImageView.tsx
@@ -1,97 +1,119 @@
-import  { useState, useCallback, useEffect } from 'react';
-import ImageViewer from 'react-simple-image-viewer';
-import { DeleteReport } from '../../ServiceActions/ReportActions';
-import { RootState } from '../../store';
-import CancelIcon from '@mui/icons-material/Cancel';
-import { useDispatch, useSelector } from 'react-redux';
-import IReportIncomingData from '../../Types/IncomingDataModels/ReportIncoming';
+import { useState, useCallback, useEffect } from "react";
+import ImageViewer from "react-simple-image-viewer";
+import { DeleteReport } from "../../ServiceActions/ReportActions";
+import { RootState } from "../../store";
+import CancelIcon from "@mui/icons-material/Cancel";
+import { useDispatch, useSelector } from "react-redux";
+import IReportIncomingData from "../../Types/IncomingDataModels/ReportIncoming";
 
 export default function ReportImageView() {
+  const dispatch = useDispatch();
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isViewerOpen, setIsViewerOpen] = useState(false);
+  const [images, setImages] = useState([""]);
+  let displayImageObjs = useSelector(
+    (state: RootState) => state.ConsultationState.currentCustomerReports
+  );
 
-    const dispatch = useDispatch();
-    const [currentImage, setCurrentImage] = useState(0);
-    const [isViewerOpen, setIsViewerOpen] = useState(false);
-    const [images, setImages] = useState([""]);
-    let displayImageObjs = useSelector((state: RootState) => state.ConsultationState.currentCustomerReports);
+  const openImageViewer = useCallback((index) => {
+    setCurrentImage(index);
+    setIsViewerOpen(true);
+  }, []);
 
-    const openImageViewer = useCallback((index) => {
-        setCurrentImage(index);
-        setIsViewerOpen(true);
-    }, []);
+  const closeImageViewer = () => {
+    setCurrentImage(0);
+    setIsViewerOpen(false);
+  };
 
-    const closeImageViewer = () => {
-        setCurrentImage(0);
-        setIsViewerOpen(false);
-    };
+  useEffect(() => {
+    getImageURLsFromReports();
+  }, [displayImageObjs]);
 
-    useEffect(() => {
-        getImageURLsFromReports()
-    }, [displayImageObjs])
+  function getImageURLsFromReports() {
+    var stringList: string[] = [];
 
-    function getImageURLsFromReports() {
-        var stringList: string[] = [];
-
-        if (displayImageObjs) {
-            displayImageObjs.forEach(element => {
-                stringList.push(element.sasUrl);
-            });
-        }
-
-        setImages(stringList);
+    if (displayImageObjs) {
+      displayImageObjs.forEach((element) => {
+        stringList.push(element.sasUrl);
+      });
     }
 
-    function deleteReport (report: IReportIncomingData) {
-        if (window.confirm("Are you sure you want to delete this prescription?")) {
-            dispatch(DeleteReport(report))
-        }
+    setImages(stringList);
+  }
+
+  function deleteReport(report: IReportIncomingData) {
+    if (window.confirm("Are you sure you want to delete this prescription?")) {
+      dispatch(DeleteReport(report));
     }
+  }
 
-    function imageViewDisplay() {
-        return (
-            <div style={{ overflow: 'auto' }}>
-                <div>
-                    {displayImageObjs && displayImageObjs.map((src, index) => (
-                        <div style={{ position: 'relative', width: 200, marginTop: 10 }}>
-                            <img
-                                src={src.sasUrl}
-                                onClick={() => openImageViewer(index)}
-                                width="200"
-                                key={index}
-                                style={{ margin: '5px' }}
-                                alt=""
-                            />
-
-                            <div onClick={() => dispatch(deleteReport(src))} style={{ position: 'absolute', top: -10, right: -15 }}>
-                                {<CancelIcon></CancelIcon>}
-                            </div>
-                        </div>
-                    ))}
-
-                    {isViewerOpen &&
-                        (<ImageViewer
-                            src={images}
-                            currentIndex={currentImage}
-                            disableScroll={false}
-                            closeOnClickOutside={true}
-                            onClose={closeImageViewer}
-                        />
-                        )}
-                </div>
-            </div>
-        );
-    }
-
-    function noReportsDisplay() {
-        return (
-            <div style={{ margin: 20, marginTop: 50, marginBottom: 50, textAlign: 'center' }}>
-                No reports yet. Upload image or Take Photo.
-            </div>
-        );
-    }
-
+  function imageViewDisplay() {
     return (
+      <div style={{ overflow: "auto" }}>
         <div>
-            {(displayImageObjs && displayImageObjs.length > 0) ? imageViewDisplay() : noReportsDisplay()}
+          {displayImageObjs &&
+            displayImageObjs.map((src, index) => (
+              <div
+                style={{
+                  display: "inline-block",
+                  position: "relative",
+                  width: 200,
+                  margin: "10px 10px 0 0",
+                }}
+              >
+                <img
+                  src={src.sasUrl}
+                  onClick={() => openImageViewer(index)}
+                  width="200"
+                  height="150"
+                  key={index}
+                  style={{ margin: "5px" }}
+                  alt=""
+                />
+
+                <div
+                  onClick={() => dispatch(deleteReport(src))}
+                  style={{ position: "absolute", top: -10, right: -15 }}
+                >
+                  <CancelIcon />
+                </div>
+              </div>
+            ))}
+
+          {isViewerOpen && (
+            <ImageViewer
+              src={images}
+              currentIndex={currentImage}
+              disableScroll={false}
+              closeOnClickOutside={true}
+              onClose={closeImageViewer}
+            />
+          )}
         </div>
+      </div>
     );
+  }
+
+  function noReportsDisplay() {
+    return (
+      <div
+        style={{
+          margin: 20,
+          marginTop: 50,
+          marginBottom: 50,
+          textAlign: "center",
+        }}
+      >
+        No reports yet. Upload image or Take Photo.
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {displayImageObjs && displayImageObjs.length > 0
+        ? imageViewDisplay()
+        : noReportsDisplay()}
+    </div>
+  );
 }
