e564a9fee281d56b4a883d48fa803eae3e9f0077
Pravallika <05b.pravallika@gmail.com>
2022-01-25 23:53:11 +0530

    search with name

--- src/Components/PatientsList/PatientsTableView.tsx
+++ src/Components/PatientsList/PatientsTableView.tsx
@@ -7,10 +7,6 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { makeStyles } from "@mui/styles";
 import ICustomerIncomingData from "../../Types/IncomingDataModels/CustomerIncoming";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../../store";
-import { CheckIfCustomerExists } from "../../ServiceActions/CustomerActions";
-import { SetAddPatientIsCheckingForCustomer, SetAddPatientPhoneNumber } from "../../Actions/AddPatientActions";
 import { Link, TableFooter, TablePagination } from "@mui/material";
 import TablePaginationActions from "../Pagination/PaginationActions";
 import usePaginationHook from "../../CustomHooks/usePaginationHook";
@@ -48,44 +44,61 @@ const usePatientTableStyles = makeStyles(() => ({
 }));
 
 export default function PatientsTableView() {
-  const dispatch = useDispatch();
   const classes = usePatientTableStyles();
 
-  const { page, rowsPerPage, handleChangePage, handleChangeRowsPerPage } = usePaginationHook(10)
-
-  const customerState = useSelector(
-    (state: RootState) => state.CustomersState
-  );
-
-  function handleCustomerSelect(customer: ICustomerIncomingData) {
-    dispatch(SetAddPatientIsCheckingForCustomer(true))
-    dispatch(CheckIfCustomerExists(customer.phoneNumbers[0].number, customer.organisationId))
-    dispatch(SetAddPatientPhoneNumber(customer.phoneNumbers[0].number))
-  }
+  const {
+    page,
+    rowsPerPage,
+    handleChangePage,
+    handleChangeRowsPerPage,
+    handleCustomerSelect,
+    search,
+    onSearch,
+    filtered,
+    customerState,
+  } = usePaginationHook();
 
   function makeCustomerListDisplay() {
-    return (rowsPerPage > 0
-      ? customerState.customers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-      : customerState.customers
-    ).map(
-      (customer: ICustomerIncomingData, index: number) => (
-        <TableRow key={customer.customerId}>
-          <TableCell align="left" onClick={() => handleCustomerSelect(customer)}><Link>{customer.firstName + " " + (customer.lastName ? customer.lastName : "")}</Link></TableCell>
-          <TableCell align="left">
-            {customer.gender}
-          </TableCell>
-          <TableCell align="left">
-            {customer.dateOfBirth.age}
-          </TableCell>
-          <TableCell onClick={() => handleCustomerSelect(customer)}><Link>{customer.phoneNumbers[0].number}</Link></TableCell>
-        </TableRow>
-      )
-    )
+    return (
+      rowsPerPage > 0
+        ? filtered.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+        : filtered
+    ).map((customer: ICustomerIncomingData, index: number) => (
+      <TableRow key={customer.customerId}>
+        <TableCell align="left" onClick={() => handleCustomerSelect(customer)}>
+          <Link>
+            {customer.firstName +
+              " " +
+              (customer.lastName ? customer.lastName : "")}
+          </Link>
+        </TableCell>
+        <TableCell align="left">{customer.gender}</TableCell>
+        <TableCell align="left">{customer.dateOfBirth.age}</TableCell>
+        <TableCell onClick={() => handleCustomerSelect(customer)}>
+          <Link>{customer.phoneNumbers[0].number}</Link>
+        </TableCell>
+      </TableRow>
+    ));
   }
 
   return (
     <div>
-      <h5 style={{ marginBottom: 20 }}>Add/Edit Patient</h5>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginRight: "5em",
+        }}
+      >
+        <h5 style={{ marginBottom: 20 }}>Add/Edit Patient</h5>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => onSearch(e.target.value)}
+        />
+      </div>
+
       <TableContainer component={Paper} style={{ borderRadius: 15 }}>
         <Table sx={{ minWidth: 700 }} aria-label="customized table">
           <TableHead>
@@ -111,14 +124,14 @@ export default function PatientsTableView() {
           <TableFooter>
             <TableRow>
               <TablePagination
-                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
                 colSpan={3}
-                count={customerState.customers.length}
+                count={filtered.length}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 SelectProps={{
                   inputProps: {
-                    'aria-label': 'rows per page',
+                    "aria-label": "rows per page",
                   },
                   native: true,
                 }}
--- src/CustomHooks/usePaginationHook.ts
+++ src/CustomHooks/usePaginationHook.ts
@@ -1,23 +1,80 @@
-import { useState } from "react";
+import { useState, useEffect, ChangeEvent } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store";
+import ICustomerIncomingData from "../Types/IncomingDataModels/CustomerIncoming";
+import { CheckIfCustomerExists } from "../ServiceActions/CustomerActions";
+import {
+  SetAddPatientIsCheckingForCustomer,
+  SetAddPatientPhoneNumber,
+} from "../Actions/AddPatientActions";
 
-export default function usePaginationHook(rows:number) {
+export default function usePaginationHook() {
+  const dispatch = useDispatch();
 
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(rows);
+  const customerState = useSelector((state: RootState) => state.CustomersState);
 
-    const handleChangePage = (
-        event: React.MouseEvent<HTMLButtonElement> | null,
-        newPage: number,
-      ) => {
-        setPage(newPage);
-      };
-    
-      const handleChangeRowsPerPage = (
-        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-      ) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-      };
-    
-    return { page, rowsPerPage, handleChangePage, handleChangeRowsPerPage }
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const [search, setSearch] = useState("");
+  const [filtered, setFiltered] = useState(customerState.customers);
+
+  useEffect(() => {
+    customerState && setFiltered(customerState.customers);
+  }, [customerState]);
+
+  function handleCustomerSelect(customer: ICustomerIncomingData) {
+    dispatch(SetAddPatientIsCheckingForCustomer(true));
+    dispatch(
+      CheckIfCustomerExists(
+        customer.phoneNumbers[0].number,
+        customer.organisationId
+      )
+    );
+    dispatch(SetAddPatientPhoneNumber(customer.phoneNumbers[0].number));
+  }
+
+  const onSearch = (value: any) => {
+    setSearch(value);
+    var filterWithPhone = customerState.customers
+      .map((customer) => [
+        customer.firstName,
+        customer.phoneNumbers.filter((number) =>
+          (number.countryCode + number.number).includes(value)
+        ),
+      ])
+      .filter((arr) => arr[1].length !== 0 && arr[0]);
+
+    var filterWithNames = customerState.customers.filter((customer) =>
+      customer.firstName.toLowerCase().includes(value.toLowerCase())
+    );
+
+    setFiltered(filterWithNames);
+  };
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return {
+    page,
+    rowsPerPage,
+    handleChangePage,
+    handleChangeRowsPerPage,
+    handleCustomerSelect,
+    search,
+    onSearch,
+    filtered,
+    customerState,
+  };
 }
