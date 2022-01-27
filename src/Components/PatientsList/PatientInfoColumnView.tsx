98feb2b37f1cd8430dbf348b34a39200f72b22b4
suryam0801 <58582997+suryam0801@users.noreply.github.com>
2022-01-26 20:57:50 -0800

    minor cleanup

--- src/Components/PatientsList/PatientInfoColumnView.tsx
+++ src/Components/PatientsList/PatientInfoColumnView.tsx
@@ -1,6 +1,8 @@
 import { Divider } from "@mui/material";
 import EditPatientView from "../EditPatient/EditPatientView";
+import AllPrescriptionsImageView from "../PrescriptionUpload/AllPrescriptionsImageView";
 import PrescriptionImageView from "../PrescriptionUpload/PrescriptionImageView";
+import AllReportsImageView from "../ReportUpload/AllReportsImageView";
 import ReportImageView from "../ReportUpload/ReportImageView";
 
 export default function PatientInfoColumnView() {
@@ -8,8 +10,8 @@ export default function PatientInfoColumnView() {
     <div>
       <h5 style={{ marginBottom: 20 }}>Add/Edit Patient</h5>
       <EditPatientView />
-      <ReportImageView />
-      <PrescriptionImageView />
+      <AllReportsImageView />
+      <AllPrescriptionsImageView />
     </div>
   );
 }
