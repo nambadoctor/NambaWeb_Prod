6f94faf6c537f9ca42c73c44d49acbb435c6008f
suryam0801 <58582997+suryam0801@users.noreply.github.com>
2022-01-27 10:59:07 -0800

    mior fix

--- src/Actions/ReportActions.ts
+++ src/Actions/ReportActions.ts
@@ -1,10 +1,10 @@
 import IReportIncomingData from "../Types/IncomingDataModels/ReportIncoming";
 
 export function FilterAllAndCurrentReports (currentAppointmentReports:IReportIncomingData[]|null, allAppointmentReports:IReportIncomingData[]) {
-    var reportsToReturn:IReportIncomingData[] = [];
+    var reportsToReturn = {} as IReportIncomingData[];
 
     if (currentAppointmentReports && currentAppointmentReports.length > 0) {
-        var currentReportIds:string[] = []
+        var currentReportIds:string[] = {} as string[]
 
         currentAppointmentReports.forEach(report => {
             currentReportIds.push(report.reportId);
