89e3b4c2ec47a84ab0e9390e9e3efbb3ea961d22
Pravallika <05b.pravallika@gmail.com>
2022-02-07 23:35:53 +0530

    side bar

--- src/Components/Consultation/ConsultationView.tsx
+++ src/Components/Consultation/ConsultationView.tsx
@@ -6,7 +6,7 @@ import PrescriptionImageView from '../PrescriptionUpload/PrescriptionImageView';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { Col, Row } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 import '../../../src/App.css';
 import { RootState } from '../../store';
 import { AllReportImagesView } from '../ReportUpload/AllReportImagesView';
@@ -16,8 +16,12 @@ import { UploadPrescriptionForConsultation } from '../../ServiceActions/Prescrip
 import NotesView from '../Notes/NotesView';
 import { GetAppointment } from '../../ServiceActions/AppointmentActions';
 import { ClearContext } from '../../Actions/ClearContextAction';
+import { Drawer, Box, List, ListItem } from '@mui/material';
+import * as Scroll from 'react-scroll';
 
 export default function ConsultationView() {
+    let LinkScroll = Scroll.Link;
+    let ElementScroll = Scroll.Element;
     const dispatch = useDispatch();
     const currentServiceProvider = useSelector(
         (state: RootState) => state.CurrentServiceProviderState.serviceProvider,
@@ -31,56 +35,120 @@ export default function ConsultationView() {
         }
     }, [currentServiceProvider, id]);
 
+    const SideBar = () => {
+        const titles = ['Patient Details', 'Reports', 'Prescriptions', 'Notes'];
+        return (
+            <div style={{ display: 'flex' }}>
+                <Drawer
+                    variant="permanent"
+                    sx={{
+                        width: 240,
+                        flexShrink: 0,
+                        [`& .MuiDrawer-paper`]: {
+                            width: 200,
+                            height: 'auto',
+                            boxSizing: 'border-box',
+                            marginTop: '5.7em',
+                        },
+                    }}
+                >
+                    <Box sx={{ overflow: 'hidden' }}>
+                        <List>
+                            {titles.map((title, i) => (
+                                <LinkScroll
+                                    activeClass="active"
+                                    to={title}
+                                    spy={true}
+                                    smooth={true}
+                                    isDynamic={true}
+                                    duration={500}
+                                    offset={-100}
+                                >
+                                    <ListItem
+                                        button
+                                        id={title}
+                                        key={i}
+                                        style={{ margin: '15px' }}
+                                    >
+                                        {title}
+                                    </ListItem>
+                                </LinkScroll>
+                            ))}
+                        </List>
+                    </Box>
+                </Drawer>
+            </div>
+        );
+    };
+
     return (
         <div>
-            <ConsultationHeader />
-            <Row style={{ margin: 20 }}>
-                <div className="blue_filled_rounded_box_top">
-                    <h3 className="blue_filled_rounded_box_top_title_item">
-                        Reports
-                    </h3>
-                    <ReportUploadPicker
-                        handlePhotoCallBack={UploadReportForConsultation}
-                        uploadButtonColor="white"
-                    />
-                </div>
-                <div className="blue_border_rounded_white_box">
-                    <ReportImageView />
-                    <AllReportImagesView
-                        showUploadButton={false}
-                        showCancelImageButton={false}
-                    />
-                </div>
-            </Row>
+            <SideBar />
+            <div style={{ marginLeft: '11em', marginBottom: '15em' }}>
+                <Row style={{ margin: 20, marginTop: 30 }}>
+                    <ElementScroll id="Patient Details" name="Patient Details">
+                        <ConsultationHeader />
+                    </ElementScroll>
+                </Row>
 
-            <Row style={{ margin: 20 }}>
-                <div className="blue_filled_rounded_box_top">
-                    <h3 className="blue_filled_rounded_box_top_title_item">
-                        Prescriptions
-                    </h3>
-                    <PrescriptionUploadPicker
-                        handlePhotoCallBack={UploadPrescriptionForConsultation}
-                        uploadButtonColor="white"
-                    />
-                </div>
-                <div className="blue_border_rounded_white_box">
-                    <PrescriptionImageView />
-                    <AllPrescriptionImagesView
-                        showUploadButton={false}
-                        showCancelImageButton={false}
-                    />
-                </div>
-            </Row>
-            <Row style={{ margin: 20 }}>
-                <div className="blue_filled_rounded_box_top">
-                    <h3 className="blue_filled_rounded_box_top_title_item">
-                        Notes
-                    </h3>
-                </div>
-                <div className="blue_border_rounded_white_box">
-                    <NotesView></NotesView>
-                </div>
-            </Row>
+                <Row style={{ margin: 20, marginTop: 30 }}>
+                    <ElementScroll id="Reports" name="Reports">
+                        <div className="blue_filled_rounded_box_top">
+                            <h3 className="blue_filled_rounded_box_top_title_item">
+                                Reports
+                            </h3>
+                            <ReportUploadPicker
+                                handlePhotoCallBack={
+                                    UploadReportForConsultation
+                                }
+                                uploadButtonColor="white"
+                            />
+                        </div>
+                        <div className="blue_border_rounded_white_box">
+                            <ReportImageView />
+                            <AllReportImagesView
+                                showUploadButton={false}
+                                showCancelImageButton={false}
+                            />
+                        </div>
+                    </ElementScroll>
+                </Row>
+
+                <Row style={{ margin: 20, marginTop: 30 }}>
+                    <ElementScroll id="Prescriptions" name="Prescriptions">
+                        <div className="blue_filled_rounded_box_top">
+                            <h3 className="blue_filled_rounded_box_top_title_item">
+                                Prescriptions
+                            </h3>
+                            <PrescriptionUploadPicker
+                                handlePhotoCallBack={
+                                    UploadPrescriptionForConsultation
+                                }
+                                uploadButtonColor="white"
+                            />
+                        </div>
+                        <div className="blue_border_rounded_white_box">
+                            <PrescriptionImageView />
+                            <AllPrescriptionImagesView
+                                showUploadButton={false}
+                                showCancelImageButton={false}
+                            />
+                        </div>
+                    </ElementScroll>
+                </Row>
+                <Row style={{ margin: 20, marginTop: 30 }}>
+                    <ElementScroll id="Notes" name="Notes">
+                        <div className="blue_filled_rounded_box_top">
+                            <h3 className="blue_filled_rounded_box_top_title_item">
+                                Notes
+                            </h3>
+                        </div>
+                        <div className="blue_border_rounded_white_box">
+                            <NotesView />
+                        </div>
+                    </ElementScroll>
+                </Row>
+            </div>
         </div>
     );
 }
