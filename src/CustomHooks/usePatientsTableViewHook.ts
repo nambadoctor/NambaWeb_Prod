0ec43e0a41b4f0d2f24aedfd7fc9c87b4ee3d422
Pravallika <05b.pravallika@gmail.com>
2022-02-10 01:06:04 +0530

    orderby name

--- src/CustomHooks/usePatientsTableViewHook.ts
+++ src/CustomHooks/usePatientsTableViewHook.ts
@@ -1,40 +1,52 @@
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../store";
-import ICustomerIncomingData from "../Types/IncomingDataModels/CustomerIncoming";
-import { SetSelectedCustomer } from "../Actions/CustomerActions";
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../store';
+import ICustomerIncomingData from '../Types/IncomingDataModels/CustomerIncoming';
+import { SetSelectedCustomer } from '../Actions/CustomerActions';
 
 export default function usePatientsTableViewHook() {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const customerState = useSelector((state: RootState) => state.CustomersState);
+    const customerState = useSelector(
+        (state: RootState) => state.CustomersState,
+    );
 
-  const [search, setSearch] = useState("");
-  const [filtered, setFiltered] = useState(customerState.customers);
+    const [search, setSearch] = useState('');
+    const [filtered, setFiltered] = useState(customerState.customers);
 
-  useEffect(() => {
-    customerState && setFiltered(customerState.customers);
-  }, [customerState]);
+    useEffect(() => {
+        let sortedList = customerState.customers.sort((a: any, b: any) => {
+            let nameA = a.firstName.toLowerCase(),
+                nameB = b.firstName.toLowerCase();
+            return nameA > nameB ? 1 : nameA < nameB ? -1 : 0;
+        });
+        customerState && setFiltered(sortedList);
+    }, [customerState]);
 
-  const onSearch = (value: any) => {
-    setSearch(value);
-    
-    var filterWithNames = customerState.customers.filter((customer) => {
-      return(
-        customer.firstName.toLowerCase().includes(value.toLowerCase()) ||
-        customer.phoneNumbers[0].number.toLowerCase().includes(value.toLowerCase()))
-    });
-    setFiltered(filterWithNames);
-  };
+    const onSearch = (value: any) => {
+        setSearch(value);
 
-  function handleCustomerSelect(customer: ICustomerIncomingData) {
-    dispatch(SetSelectedCustomer(customer))
-  }
+        var filterWithNames = customerState.customers.filter((customer) => {
+            return (
+                customer.firstName
+                    .toLowerCase()
+                    .includes(value.toLowerCase()) ||
+                customer.phoneNumbers[0].number
+                    .toLowerCase()
+                    .includes(value.toLowerCase())
+            );
+        });
+        setFiltered(filterWithNames);
+    };
 
-  return {
-    search,
-    onSearch,
-    handleCustomerSelect,
-    filtered,
-  };
+    function handleCustomerSelect(customer: ICustomerIncomingData) {
+        dispatch(SetSelectedCustomer(customer));
+    }
+
+    return {
+        search,
+        onSearch,
+        handleCustomerSelect,
+        filtered,
+    };
 }
