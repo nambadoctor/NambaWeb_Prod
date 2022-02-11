994a1109849687d3034afefa875bf4956d900246
Pravallika <05b.pravallika@gmail.com>
2022-02-12 04:03:20 +0530

    login ang otp

--- src/ServiceActions/LoginActions.ts
+++ src/ServiceActions/LoginActions.ts
@@ -1,45 +1,49 @@
-import { signInWithPhoneNumber, getAuth, RecaptchaVerifier } from "@firebase/auth";
-import { clearAuthToken } from "../Auth/FirebaseUserInfoHelper";
+import {
+    signInWithPhoneNumber,
+    getAuth,
+    RecaptchaVerifier,
+    ConfirmationResult,
+    UserCredential,
+} from '@firebase/auth';
+import { clearAuthToken } from '../Auth/FirebaseUserInfoHelper';
+
+let confirmationR: ConfirmationResult | null = null;
+let userCredential: UserCredential | null | 'wrong otp' = null;
 
 function getAppVerifier() {
-    return new RecaptchaVerifier('sign-in-button', {
-        'size': 'invisible',
-        'callback': (response: any) => {
-            // reCAPTCHA solved, allow signInWithPhoneNumber.
-            //onSignInSubmit();
-        }
-    }, getAuth());
+    return new RecaptchaVerifier(
+        'sign-in-button',
+        {
+            size: 'invisible',
+            callback: (response: any) => {
+                // reCAPTCHA solved, allow signInWithPhoneNumber.
+                //onSignInSubmit();
+            },
+        },
+        getAuth(),
+    );
 }
 
-export default function useAuthHook() {
-    async function SignInWithPhoneNumberHelper(phoneNumber: string) {
-        await signInWithPhoneNumber(getAuth(), phoneNumber, getAppVerifier())
-            .then(function (confirmationResult) {
-                var verificationCode = window.prompt('Please enter the verification ' +
-                    'code that was sent to your mobile device.');
+export const SignInWithPhoneNumberHelper = async (phoneNumber: string) => {
+    await signInWithPhoneNumber(getAuth(), phoneNumber, getAppVerifier()).then(
+        function (confirmationResult) {
+            confirmationR = confirmationResult;
+        },
+    );
+    return confirmationR?.verificationId;
+};
 
-                var confirmationResultTemp = confirmationResult.confirm(verificationCode!).then((result) => {
-                    // User signed in successfully.
-                    // ...
-                }).catch((error) => {
-                    // User couldn't sign in (bad verification code?)
-                    // ...
-                });
+export const VerifyOtp = (otp: string) => {
+    confirmationR &&
+        confirmationR.confirm(otp).then((result) => {
+            userCredential = result !== null ? result : 'wrong otp';
+        });
+    return userCredential;
+};
 
-                return confirmationResultTemp;
-            })
-            .catch(function (error) {
-                // Handle Errors here.
-            });
+export function SignOut() {
+    if (window.confirm('Are you sure you want to logout?')) {
+        clearAuthToken();
+        getAuth().signOut();
     }
-
-    function SignOut() {
-        if (window.confirm("Are you sure you want to logout?")) {
-            clearAuthToken()
-            getAuth().signOut();
-        }
-    }
-
-    return { SignInWithPhoneNumberHelper, SignOut };
 }
-
