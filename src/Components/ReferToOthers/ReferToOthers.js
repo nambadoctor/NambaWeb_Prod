d39aa350945be311c8f5aee5fe5d364d1db64bab
momrider69 <jacob.abraham@claystack.com>
2022-03-28 09:59:38 +0530

    Fix build issues

--- src/Components/ReferToOthers/ReferToOthers.js
+++ src/Components/ReferToOthers/ReferToOthers.js
@@ -12,9 +12,9 @@ import { ReferPatient } from '../../ServiceActions/ReferralActions';
 
 //TEMP HACK REFERRAL DATA
 const sujathaReferals = [
-    { value: 'sujatha-kousalya', label: 'Kousalya', number: "7530043008" },
-    { value: 'sujatha-rajesh', label: 'Rajesh', number: "7530043008" },
-    { value: 'sujatha-manivannan', label: 'Manivannan', number: "7530043008" },
+    { value: 'sujatha-kousalya', label: 'Kousalya', number: '7530043008' },
+    { value: 'sujatha-rajesh', label: 'Rajesh', number: '7530043008' },
+    { value: 'sujatha-manivannan', label: 'Manivannan', number: '7530043008' },
 ];
 //END DATA
 
@@ -33,7 +33,13 @@ export function ReferToOthers() {
             reasonForReferral: Yup.string(),
         }),
         onSubmit: (values) => {
-            dispatch(ReferPatient(selectedOption.number, values.reasonForReferral, selectedOption.label))
+            dispatch(
+                ReferPatient(
+                    selectedOption.number,
+                    values.reasonForReferral,
+                    selectedOption.label,
+                ),
+            );
             formik.resetForm();
             setSelectedOption(null);
         },
@@ -48,45 +54,47 @@ export function ReferToOthers() {
                     <h5>Refer this patient </h5>
 
                     <Row>
-                        <div style={{display:"flex"}}>
-                        <TextField
-                            fullWidth
-                            label="Reason For Referral"
-                            placeholder="Reason For Referral"
-                            margin="normal"
-                            size="small"
-                            {...formik.getFieldProps('reasonForReferral')}
-                            helperText={
-                                formik.touched.reasonForReferral &&
-                                formik.errors.reasonForReferral
-                            }
-                            error={
-                                formik.touched.reasonForReferral &&
-                                !!formik.errors.reasonForReferral
-                            }
-                            inputProps={{ maxLength: 28 }}
+                        <div style={{ display: 'flex' }}>
+                            <TextField
+                                fullWidth
+                                label="Reason For Referral"
+                                placeholder="Reason For Referral"
+                                margin="normal"
+                                size="small"
+                                {...formik.getFieldProps('reasonForReferral')}
+                                helperText={
+                                    formik.touched.reasonForReferral &&
+                                    formik.errors.reasonForReferral
+                                }
+                                error={
+                                    formik.touched.reasonForReferral &&
+                                    !!formik.errors.reasonForReferral
+                                }
+                                inputProps={{ maxLength: 25 }}
                             />
-                            <div style={{marginTop: 12, marginLeft: 10}}>
-                            {formik.values.reasonForReferral.length}/28 characters
+                            <div style={{ marginTop: 12, marginLeft: 10 }}>
+                                {formik.values.reasonForReferral.length}/25
+                                characters
                             </div>
                         </div>
                     </Row>
 
                     <div>
                         <Select
-                        defaultValue={selectedOption}
-                        onChange={setSelectedOption}
-                        options={sujathaReferals}
-                        placeholder="Refer To"
-                    />
-
-                    <CustomButton
-                        type="submit"
-                        onClick={formik.handleSubmit}
-                        isPurple={false}
-                        title="Save"
-                        small={false}
-                    />
+                            defaultValue={selectedOption}
+                            onChange={setSelectedOption}
+                            options={sujathaReferals}
+                            placeholder="Refer To"
+                        />
+                        <div style={{ marginTop: '8px' }}>
+                            <CustomButton
+                                type="submit"
+                                onClick={formik.handleSubmit}
+                                isPurple={false}
+                                title="Save"
+                                small={false}
+                            />
+                        </div>
                     </div>
                 </div>
             )}
